85c05c0a863f3cb3d3e71b7f5a415ecbbdd1cbc7
Vinicius Alexandre Stresser <78008324+vinn4@users.noreply.github.com>
2022-03-10 23:54:30 -0300

    v-10-03-2022
    Novas atividades realizadas com o curso.

--- ajax/server.js
+++ ajax/server.js
@@ -1,10 +1,52 @@
-const { urlencoded } = require('body-parser')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser,urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
-app.listen(8080,() => console.log('executando...'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+const upload = multer({
+    storage
+}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Erro blerg')
+        }
+
+        res.end('Concluido')
+    })
+})
+
+
+app.post('/formulario', (req,res) =>{
+    res.send({
+        ...req.body,
+        id: 7
+    })
+})
+
+
+app.get('/parOuImpar', (req, res) =>{
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ?'par' : 'impar'
+    })
+})
+
+
+app.listen(8080, () => console.log('executando...'))

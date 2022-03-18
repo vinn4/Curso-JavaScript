2131bf125df53412624305651953a8b5213cf400
Vinicius Alexandre Stresser <78008324+vinn4@users.noreply.github.com>
2022-03-17 21:46:35 -0300

    v-17-03-2022
    v-17-03-2022

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const {series, parallel} = require('gulp')
+
+const antes1 = cb =>{
+    console.log('tarefa antes1')
+    return cb()
+}
+const antes2 = cb =>{
+    console.log('tarefa antes2')
+    return cb()
+}
+
+function copiar (cb){
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+    .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+const fim = cb =>{
+    console.log('tarefa fim')
+    return cb()
+}
+
+
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim)

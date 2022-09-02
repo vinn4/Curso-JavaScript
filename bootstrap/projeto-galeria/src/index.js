1d33bb29e278a9c301be81ed444a4e60494fc6b7
Vinicius Alexandre Stresser <78008324+vinn4@users.noreply.github.com>
2022-09-01 21:48:45 -0300

    v-01-09-2022
    v-01-09-2022

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// arquivos SASS
+import "./scss/index.scss"
+
+//dependencias
+import 'jquery'
+import 'bootstrap'
+
+//meus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'

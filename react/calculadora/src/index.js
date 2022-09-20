f7bc561070cffdf1febd64694dd4ac86bc86c51b
Vinicius Alexandre Stresser <78008324+vinn4@users.noreply.github.com>
2022-09-20 19:14:20 -0300

    v-19-09-2022
    v-19-09-2022

---
+++ react/calculadora/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <div>
+  <h1>Calculadora</h1>
+  <Calculator></Calculator>
+  </div>
+);
+
+
+reportWebVitals();
